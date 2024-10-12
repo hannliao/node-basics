c703c4e9d711e31380eb67bad7ea6f41baa3205e
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-10-12 14:36:28 -0600

    Rewrite using Express

---
+++ app.js
@@ -0,0 +1,18 @@
+const PORT = 8080;
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.get('/about', (req, res) =>
+  res.sendFile(path.join(__dirname, './about.html'))
+);
+app.get('/contact-me', (req, res) =>
+  res.sendFile(path.join(__dirname, './contact-me.html'))
+);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, './404.html'));
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
